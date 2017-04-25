c945fa20fbf939963663cafdd9368dee72d4f1f6
Jason Zhang <jaz998@users.noreply.github.com>
2017-04-25 18:13:34 +1000

    unlike post validation done

--- handlers/unlikepost.py
+++ handlers/unlikepost.py
@@ -1,12 +1,14 @@
 from handlers.mainhandler import MainHandler
 from google.appengine.ext import ndb
 import modules.parent_keys as parent_key
+from modules.myownvalidations import user_can_like_post
 
 
 
 class  UnlikePost(MainHandler):
+	@user_can_like_post
 	def post(self):
-		post_id = self.request.get("post_id")
+		post_id = self.request.get("post_id_value")
 		user_id = self.request.get("user_id")
 		keyPost = ndb.Key('Post', int(post_id), parent=parent_key.blog_key())
 		post = keyPost.get()
