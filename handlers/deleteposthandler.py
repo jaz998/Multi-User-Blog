f9025d2d285ad9408aaf05add051cfc4929b559b
Jason Zhang <jaz998@users.noreply.github.com>
2017-04-25 17:08:55 +1000

    delete post validation done

--- handlers/deleteposthandler.py
+++ handlers/deleteposthandler.py
@@ -1,10 +1,12 @@
 from handlers.mainhandler import MainHandler
 from google.appengine.ext import ndb
 import modules.parent_keys as parent_key
+from modules.myownvalidations import user_owns_post
 
 class DeletePost(MainHandler):
+	@user_owns_post
 	def post(self):
-		post_id = self.request.get("post_id")
+		post_id = self.request.get("post_id_value")
 		key = ndb.Key('Post', int(post_id), parent=parent_key.blog_key())
 		post = key.get()
 		subject = post.subject
